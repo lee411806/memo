3f38ad9f8d6a33f053d6a2b1d7a399e4f98c5393
JDragon <lee411806@naver.com>
2024-08-29 19:42:56 +0900

    Query Methods란 무엇인가?

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -4,7 +4,10 @@ import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-
+    List<Memo> findAllByOrderByModifiedAtDesc();
+    ;
 }
